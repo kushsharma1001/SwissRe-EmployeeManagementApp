61af7c416e0122ab5daa6c663574ef24210bda0e
Kush Sharma <kushsharma1001@gmail.com>
2025-07-04 01:15:13 +0530

    Add files via upload

--- app/src/main/java/com/swissre/app/util/Constants.java
+++ app/src/main/java/com/swissre/app/util/Constants.java
@@ -9,12 +9,6 @@ public class Constants {
      * This variable is used to read employee data from 1st row of csv file as 0th row in the csv file is for header.
      */
     public static final int EMPLOYEE_DATA_ROW_NUM_START_INDEX = 1;
-
-    public static final String IS_UNDERPAID_BY = " is underpaid by: ";
-
-    public static final String IS_OVERPAID_BY = " is overpaid by: ";
-
-    public static final String HAS_A_REPORTING_DEPTH_OF_TEXT = " has a reporting depth of ";
-    public static final String CSV_FILE_READ_ERROR = "Error reading file at src/main/resources/ directory with name: \"employees.csv\". Make sure that file exists in this location with the same name.";
     public static final String CSV_DATA_FILE_NAME = "employees.csv";
+    public static final String CSV_FILE_READ_ERROR = "Error reading file from src/main/resources directory with name: \"" + CSV_DATA_FILE_NAME + "\". Make sure that file exists in this location with the same name.";
 }
